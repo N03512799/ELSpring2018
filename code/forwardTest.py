faf97f30351aa5aa3d56c55a68f8d10adb0b82e0
N03512799 <dmitries1@hawkmail.newpaltz.edu>
2018-05-12 14:29:34 -0500

    update forward coding

--- code/forwardTest.py
+++ code/forwardTest.py
@@ -4,11 +4,17 @@ from Line_Follower import Line_Follower
 
 lf = Line_Follower()
 test = []
-base_speed = 30
-new_speed = 45
+
+#declare duty cycles used for PWM control
+
+slow_speed = 30
+base_speed = 40
+new_speed = 55
  
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
+
+#Set GPIO pin assignments for motor control
  
 Motor1A = 16
 Motor1B = 18
@@ -25,6 +31,8 @@ Motor3E = 40
 Motor4A = 33
 Motor4B = 35
 Motor4E = 37
+
+#Set all pins for GPIO output
  
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
@@ -42,19 +50,25 @@ GPIO.setup(Motor4A,GPIO.OUT)
 GPIO.setup(Motor4B,GPIO.OUT)
 GPIO.setup(Motor4E,GPIO.OUT)
 
+#Declare variables for PWM control of enable lines
+
 p1 = GPIO.PWM(Motor1E, 200)
 p2 = GPIO.PWM(Motor2E, 200)
 p3 = GPIO.PWM(Motor3E, 200)
 p4 = GPIO.PWM(Motor4E, 200)
+
+#Set initial PWM controls to 0 for no movement
 
 p1.start(0)
 p2.start(0)
 p3.start(0)
 p4.start(0)
 
-intersection = True
+#loop movement until intersection of dead end is reached
 
-while intersection:
+while True:
+
+#Set initial forward movement conditions
 
 	GPIO.output(Motor1A,GPIO.LOW)
 	GPIO.output(Motor1B,GPIO.HIGH)
@@ -72,11 +86,12 @@ while intersection:
 	GPIO.output(Motor4B,GPIO.LOW)
 	p4.ChangeDutyCycle(base_speed)
 
+#Use test to read data from IR sensor
+
 	test = lf.read_digital()
-#	print test
 
 
-#increase PWM for left side to correct straight path
+#increase PWM for left side and decrease for right to correct path
 	
 	count = 0
 	
@@ -86,9 +101,15 @@ while intersection:
 		
 		p1.ChangeDutyCycle(new_speed)
 
+		p2.ChangeDutyCycle(slow_speed)
+
+		p3.ChangeDutyCycle(slow_speed)
+
 		p4.ChangeDutyCycle(new_speed)
 
-#check for intersections
+#check for intersections, counts used to avoid false positives
+
+#check for left hand turn
 
 		if test[0] == 1:
 
@@ -113,6 +134,10 @@ while intersection:
 
 	p1.ChangeDutyCycle(base_speed)
 
+	p2.ChangeDutyCycle(base_speed)
+
+	p3.ChangeDutyCycle(base_speed)
+
 	p4.ChangeDutyCycle(base_speed)
 
 	if count > 10:
@@ -121,17 +146,23 @@ while intersection:
 
 	count = 0
 
-#increase PWM for right side to correct straight path
+#increase PWM for right side and decrease left side to correct straight path
 	
 	while test[3]:
 
 		test = lf.read_digital()
 
+		p1.ChangeDutyCycle(slow_speed)
+
 		p2.ChangeDutyCycle(new_speed)
 
 		p3.ChangeDutyCycle(new_speed)
 
-#check for intersections
+		p4.ChangeDutyCycle(slow_speed)
+
+#check for intersections, counts used to avoid false positives
+
+#check for left hand turn
 
 		if test[0] == 1:
 
@@ -152,15 +183,21 @@ while intersection:
 
 			break
 
+#Return to base speeds
+
+	p1.ChangeDutyCycle(base_speed)
+
 	p2.ChangeDutyCycle(base_speed)
 
 	p3.ChangeDutyCycle(base_speed)
+
+	p4.ChangeDutyCycle(base_speed)
 
 	if count > 10:
 
 		break
 
-#check for intersections
+#check for intersections if path is currently not being corrected
 
 	if test[0] == 1:
 
@@ -187,21 +224,27 @@ while intersection:
 
 	count = 0
 
+#check for desd end
+
 	while test[2] != 1:
 		
 		count = count + 1
 		test = lf.read_digital()
 
-		if count == 40:
+		if count == 20:
 
 			break
-	if count == 40:
+	if count == 20:
 		break
+
+#stop motor
 
 p1.stop()
 p2.stop()
 p3.stop()
 p4.stop()
+
+# wait 1 sec to take additional reading to determine path
 
 sleep(1)
 
@@ -209,5 +252,7 @@ print test
 
 test1 = lf.read_digital()
 print test1
+
+#cleanup pin assignments
 
 GPIO.cleanup()
